b90e1468d4339d4b6f23fd5df0cb08762e48000c
Jose Morales <josmliraa@gmail.com>
2026-03-07 17:53:26 -0600

    feat: Add project references

--- LibraryFlow.API/obj/Debug/net8.0/LibraryFlow.API.AssemblyInfo.cs
+++ LibraryFlow.API/obj/Debug/net8.0/LibraryFlow.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06ceeacbc34d941264724b868068b3a1326a3f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08262cad82fc6a60ffb12e87a106e3ed3c839e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
