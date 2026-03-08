a42370a6f2ce70b0fac42af2da8420da5b66620e
Jose Morales <josmliraa@gmail.com>
2026-03-07 22:13:27 -0600

    feat: Add Reservation DTO

---
+++ LibraryFlow.Application/DTOs/ReservacionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Application.DTOs
+{
+    public class ReservacionDto
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string TituloLibro { get; set; }
+        public DateTime FechaReservacion { get; set; }
+    }
+}
