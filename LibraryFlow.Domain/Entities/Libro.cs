ea0cd0e68fbd21324ec7fbb31aea2e4286671f45
Jose Morales <josmliraa@gmail.com>
2026-03-07 21:14:59 -0600

    feat: Add entities to Domain project

---
+++ LibraryFlow.Domain/Entities/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Domain.Entities
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int StockDisponible { get; set; }
+        public List<Reservacion> Reservaciones { get; set; } = new();
+    }
+}
---
+++ LibraryFlow.Domain/Entities/Reservacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Domain.Entities
+{
+    public class Reservacion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Libro")]
+        public int IdLibro { get; set; }
+        public string NombreUsuario { get; set; }
+        public DateTime FechaReservacion { get; set; }
+
+        public Libro Libro { get; set; }
+    }
+}
