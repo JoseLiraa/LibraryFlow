2e6b3cf6205f242f1ad78e9428ed619ed0923c3e
Jose Morales <josmliraa@gmail.com>
2026-03-07 21:42:04 -0600

    feat: Add repository pattern interfaces

---
+++ LibraryFlow.Application/Interfaces/ILibroRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Application.Interfaces
+{
+    public interface ILibroRepository
+    {
+        Task<List<Libro>> ObtenerTodosAsync();
+        Task<Libro?> ObtenerPorIdAsync(int id);
+        Task ActualizarLibroAsync(Libro libro);
+    }
+}
---
+++ LibraryFlow.Application/Interfaces/IReservacionRepository.cs
@@ -0,0 +1,15 @@
+﻿using LibraryFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Application.Interfaces
+{
+    public interface IReservacionRepository
+    {
+        Task CrearReservacionAsync(Reservacion reservacion);
+        Task<List<Reservacion>> ObtenerTodosAsync();
+    }
+}
