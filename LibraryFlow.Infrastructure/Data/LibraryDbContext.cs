028c1d48bb7045f110b716ef298ab9c462126c53
Jose Morales <josmliraa@gmail.com>
2026-03-07 21:18:33 -0600

    feat: Add DbContext

---
+++ LibraryFlow.Infrastructure/Data/LibraryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LibraryFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Infrastructure.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Reservacion> Reservaciones { get; set; }
+    }
+}
