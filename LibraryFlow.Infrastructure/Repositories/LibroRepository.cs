baaf2ff85a826dfb5583991036e8ab4010260f18
Jose Morales <josmliraa@gmail.com>
2026-03-07 22:22:07 -0600

    feat: Add book and reservation repository

---
+++ LibraryFlow.Infrastructure/Repositories/LibroRepository.cs
@@ -0,0 +1,38 @@
+﻿using LibraryFlow.Application.Interfaces;
+using LibraryFlow.Domain.Entities;
+using LibraryFlow.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Infrastructure.Repositories
+{
+    public class LibroRepository : ILibroRepository
+    {
+        private readonly LibraryDbContext _context;
+
+        public LibroRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Libro>> ObtenerTodosAsync()
+        {
+            return await _context.Libros.ToListAsync();
+        }
+
+        public async Task<Libro?> ObtenerPorIdAsync(int id)
+        {
+            return await _context.Libros.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task ActualizarLibroAsync(Libro libro)
+        {
+            _context.Libros.Update(libro);
+            await _context.SaveChangesAsync();
+        }
+    }
+ }
---
+++ LibraryFlow.Infrastructure/Repositories/ReservacionRepository.cs
@@ -0,0 +1,36 @@
+﻿using LibraryFlow.Application.DTOs;
+using LibraryFlow.Application.Interfaces;
+using LibraryFlow.Domain.Entities;
+using LibraryFlow.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Infrastructure.Repositories
+{
+    public class ReservacionRepository : IReservacionRepository
+    {
+        private readonly LibraryDbContext _context;
+
+        public ReservacionRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CrearReservacionAsync(Reservacion reservacion)
+        {
+            await _context.Reservaciones.AddAsync(reservacion);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Reservacion>> ObtenerTodosAsync()
+        {
+            return await _context.Reservaciones
+                .Include(x => x.Libro)
+                .ToListAsync();
+        }
+    }
+}
