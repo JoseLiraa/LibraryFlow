e1b016f324e08a776a5718a29d813e2644f709e1
Jose Morales <josmliraa@gmail.com>
2026-03-07 21:25:14 -0600

    feat: Add DTOs

---
+++ LibraryFlow.Application/DTOs/CrearReservacionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Application.DTOs
+{
+    public class CrearReservacionDto
+    {
+        public int IdLibro { get; set; }
+        public string NombreUsuario { get; set; }
+    }
+}
---
+++ LibraryFlow.Application/DTOs/LibroDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Application.DTOs
+{
+    public class LibroDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Stock { get; set; }
+    }
+}
