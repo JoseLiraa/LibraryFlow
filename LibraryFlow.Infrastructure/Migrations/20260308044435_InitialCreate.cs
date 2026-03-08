97b68f5afccfd6f829ac54c3bd40eea426ea2612
Jose Morales <josmliraa@gmail.com>
2026-03-08 00:35:46 -0600

    feat: Add initial entity framework migrations

---
+++ LibraryFlow.Infrastructure/Migrations/20260308044435_InitialCreate.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StockDisponible = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdLibro = table.Column<int>(type: "int", nullable: false),
+                    NombreUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaReservacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservaciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservaciones_Libros_IdLibro",
+                        column: x => x.IdLibro,
+                        principalTable: "Libros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Libros",
+                columns: new[] { "Id", "Autor", "StockDisponible", "Titulo" },
+                values: new object[,]
+                {
+                    { 1, "Stephen Hawking", 5, "La teoría del todo" },
+                    { 2, "James Clear", 2, "Hábitos Atómicos" },
+                    { 3, "Dori Sánchez", 3, "Soltar para avanzar" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservaciones_IdLibro",
+                table: "Reservaciones",
+                column: "IdLibro");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservaciones");
+
+            migrationBuilder.DropTable(
+                name: "Libros");
+        }
+    }
+}
