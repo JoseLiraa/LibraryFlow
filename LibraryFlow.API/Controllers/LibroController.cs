5f42ab8d86ac28b15b9213ef96182848285aa81e
Jose Morales <josmliraa@gmail.com>
2026-03-07 22:42:41 -0600

    feat: Add endpoint postreservations and getbooks

---
+++ LibraryFlow.API/Controllers/LibroController.cs
@@ -0,0 +1,34 @@
+﻿using LibraryFlow.Application.Services;
+using LibraryFlow.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace LibraryFlow.API.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class LibroController : ControllerBase
+    {
+        private readonly LibroService _libroService;
+
+        public LibroController(LibroService libroService)
+        {
+            _libroService = libroService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerLibros()
+        {
+            try
+            {
+                var libros = await _libroService.ObtenerLibros();
+                return Ok(libros);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensaje = ex.Message });
+            }
+        }
+    }
+}
---
+++ LibraryFlow.API/Controllers/ReservacionController.cs
@@ -0,0 +1,32 @@
+﻿using LibraryFlow.Application.DTOs;
+using LibraryFlow.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryFlow.API.Controllers
+{
+    [ApiController]
+    [Route("api/reservationes")]
+    public class ReservacionController : ControllerBase
+    {
+        private readonly ReservacionService _reservacionService;
+
+        public ReservacionController(ReservacionService reservacionService)
+        {
+            _reservacionService = reservacionService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearReserva(CrearReservacionDto dto)
+        {
+            try
+            {
+                await _reservacionService.CrearReservacion(dto.IdLibro, dto.NombreUsuario);
+                return Ok("Reservación creada con éxito");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensaje = ex.Message });
+            }
+        }
+    }
+}
