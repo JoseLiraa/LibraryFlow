5f55263428899b69577ca6ed4293e4a2e3899a3e
Jose Morales <josmliraa@gmail.com>
2026-03-07 22:27:03 -0600

    feat: Add reservation service

---
+++ LibraryFlow.Application/Services/ReservacionService.cs
@@ -0,0 +1,44 @@
+﻿using LibraryFlow.Application.DTOs;
+using LibraryFlow.Application.Interfaces;
+using LibraryFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFlow.Application.Services
+{
+    public class ReservacionService
+    {
+        private readonly ILibroRepository _libroRepository;
+        private readonly IReservacionRepository _reservacionRepository;
+
+        public ReservacionService(ILibroRepository libroRepository,
+            IReservacionRepository reservacionRepository)
+        {
+            _libroRepository = libroRepository;
+            _reservacionRepository = reservacionRepository;
+        }
+
+        public async Task CrearReservacion(int idLibro, string nombreUsuario)
+        {
+            var libro = await _libroRepository.ObtenerPorIdAsync(idLibro)
+                ?? throw new Exception("Libro no encontrado");
+
+            if (libro.StockDisponible <= 0) { throw new Exception("No hay stock disponible"); }
+
+            libro.StockDisponible -= 1;
+
+            var reservacion = new Reservacion
+            {
+                IdLibro = idLibro,
+                NombreUsuario = nombreUsuario,
+                FechaReservacion = DateTime.UtcNow
+            };
+
+            await _reservacionRepository.CrearReservacionAsync(reservacion);
+            await _libroRepository.ActualizarLibroAsync(libro);
+        }
+    }
+}
